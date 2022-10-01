b53b1808b36c7a4f638a539772c5705b2d7ab5f5
anajlaoui007 <anajlaoui007@gmail.com>
2022-10-01 11:24:57 +0100

    add folder of mongose

---
+++ models/person.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const personSchema = new mongoose.Schema({
+  name: { type: String, required: true, uppercase: true },
+  age: Number,
+  favoriteFoods: [String],
+});
+const Person = mongoose.model("person", personSchema);
+module.exports = Person;
